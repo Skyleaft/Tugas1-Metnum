9ceae9d50696276fa4205181b63da91738293daa
Milzan Malik <38238691+Skyleaft@users.noreply.github.com>
2020-04-17 16:54:32 +0700

    comment main.py

--- Main.py
+++ Main.py
@@ -5,13 +5,14 @@ from tabulate import tabulate
 from Script import bisection_regulafalsi
 from Script import newton_secant
 
-
+#f = soal f(x)= x^3 + 5x^2 - 10x - 4
 f = lambda x: x**3 + 5  * x**2 - 10 * x - 4
-#bisection_regulafalsi.cari_akar(f,1.3,2,0.0001,'bisection')
+#masukan kedalam method cari akar dengan parameter (f(x), interval awal, interval akhir, toleransi error, method nya(bisection/regula_falsi))
+bisection_regulafalsi.cari_akar(f,1.3,2,0.0001,'bisection')
+bisection_regulafalsi.cari_akar(f,1.3,2,0.0001,'regula_falsi')
 
-#bisection_regulafalsi.cari_akar(f,1.3,2,0.0001,'regula_falsi')
-
+#fd=turunan f(x)
 fd = lambda x: 3 * x**2 + 10 * x - 10
+#masukan kedalam method cari akar dengan parameter(f(x), toleransi error, interval awal(tambahkan interval akhir jika method secant), turunan f(x), method nya)
 newton_secant.cari_akar(f, 0.0001,[1.3], fd, 'newton')
-
 newton_secant.cari_akar(f, 0.0001,[1.3, 2],method='secant')
